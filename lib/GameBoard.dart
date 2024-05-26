04b50c92eebb2e117b883f8318aa7260cda4d31f
Youssef Mohamed <79748625+YoussefMohamed7557@users.noreply.github.com>
2024-05-26 13:11:38 +0300

    design the structure of the screen without Styling

--- lib/GameBoard.dart
+++ lib/GameBoard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/material.dart ';
 class GameBoard extends StatelessWidget {
   static const routeName = "GameBoard";
@@ -5,7 +6,74 @@ class GameBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Center(
+          child: Text("XO Game"),
+        ),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment:MainAxisAlignment.center,
+                children: [
+                Text("player1 (X)"),
+                Text("0"),
+              ],),
+              Column(
+                mainAxisAlignment:MainAxisAlignment.center,
+                children: [
+                Text("player2 (O)"),
+                Text("0"),
+              ],)
+            ],
+          )),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+                Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text(""))),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
