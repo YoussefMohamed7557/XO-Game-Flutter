3575c88ad6a751f843c44b16679f92ce97de60d3
Youssef Mohamed <79748625+YoussefMohamed7557@users.noreply.github.com>
2024-05-28 12:09:52 +0300

    add alert and some style effects to notify winner

--- lib/ColorsAsset.dart
+++ lib/ColorsAsset.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 class ColorsAsset {
   static final unClickedColor = Color.fromRGBO(100, 100, 100, 1);
   static final clickedColor = Color.fromRGBO(68, 134, 248, 1.0);
-  static final winnerColor = Color.fromRGBO(86, 246, 3, 1.0);
+  static final winnerColor = Colors.red;
 }
